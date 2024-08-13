a7ae8cc98f70bf227df9f318391765aa84eff867
haolin <lwang0101@outlook.com>
2024-08-13 14:28:52 +0100

    Leftnet - change trainer to basenn

--- examples/leftnet_step_by_step/main.py
+++ examples/leftnet_step_by_step/main.py
@@ -1,98 +1,159 @@
-"""
-Codes borrowed from Open Catalyst Project (OCP) https://github.com/Open-Catalyst-Project/ocp (MIT license)
-"""
-
-import copy
+import argparse
 import logging
 
-import submitit
+import pytorch_lightning as pl
+from torch.utils.data.dataloader import DataLoader
 
-from m2models.common.flags import flags
-from m2models.common.utils import (
-    build_config,
-    create_grid,
-    new_trainer_context,
-    save_experiment_log,
-    setup_logging,
-)
+from examples.leftnet_step_by_step.m2models.common.data_parallel import ParallelCollater
+from examples.leftnet_step_by_step.m2models.datasets.lmdb_dataaccess import LmdbDatasetAccess
+# from torch_geometric.data import DataLoader
 
-import random
-import torch
+# from config import get_cfg_defaults
+# from model import get_model
+from get_model import load_model
+# from data import get_data
+# from data import Data
+# from data import *
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint, TQDMProgressBar
 
-class Runner(submitit.helpers.Checkpointable):
-    def __init__(self):
-        self.config = None
+import yaml
 
-    def __call__(self, config):
-        with new_trainer_context(args=args, config=config) as ctx:
-            self.config = ctx.config # dataset is defined here
-            self.task = ctx.task
-            self.trainer = ctx.trainer
+# embeddings
+# from qmof_khot_embeddings import QMOF_KHOT_EMBEDDINGS
+# from khot_embeddings import KHOT_EMBEDDINGS
 
-            self.task.setup(self.trainer)
-            self.task.run()
 
-    def checkpoint(self, *args, **kwargs):
-        new_runner = Runner()
-        self.trainer.save(checkpoint_file="checkpoint.pt", training_state=True)
-        self.config["checkpoint"] = self.task.chkpt_path
-        self.config["timestamp_id"] = self.trainer.timestamp_id
-        if self.trainer.logger is not None:
-            self.trainer.logger.mark_preempting()
-        return submitit.helpers.DelayedSubmission(new_runner, self.config)
+# from kale.loaddata.video_access import VideoDataset
+# from kale.loaddata.video_multi_domain import VideoMultiDomainDatasets
+from kale.utils.seed import set_seed
+# from lmdb_data import LmdbDataset
+# import numpy as np
+# from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+def arg_parse():
+    """Parsing arguments"""
+    parser = argparse.ArgumentParser(description="Domain Adversarial Networks on Action Datasets")
+    parser.add_argument("--cfg", required=True, help="path to config file", type=str)
+    parser.add_argument(
+        "--devices",
+        default=1,
+        help="gpu id(s) to use. int(0) for cpu. list[x,y] for xth, yth GPU."
+        "str(x) for the first x GPUs. str(-1)/int(-1) for all available GPUs",
+    )
+    parser.add_argument("--resume", default="", type=str)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """The main for this domain adaptation example, showing the workflow"""
+    args = arg_parse()
+
+    # ---- setup configs ----
+    # cfg = get_cfg_defaults()
+    cfg = yaml.safe_load(open(args.cfg, 'r'))
+    # cfg.merge_from_file(args.cfg)
+    # cfg.freeze()
+    print(cfg)
+
+    # ---- setup output ----
+    format_str = "@%(asctime)s %(name)s [%(levelname)s] - (%(message)s)"
+    logging.basicConfig(format=format_str)
+    # ---- setup dataset ----
+    # seed = cfg["model"]["seed"] if cfg["model"]["seed"] else 2020
+    seed = 2020
+    # source, target, num_classes = VideoDataset.get_source_target(
+    #     VideoDataset(cfg.DATASET.SOURCE.upper()), VideoDataset(cfg.DATASET.TARGET.upper()), seed, cfg
+    # )
+    # dataset = VideoMultiDomainDatasets(
+    #     source,
+    #     target,
+    #     image_modality=cfg.DATASET.IMAGE_MODALITY,
+    #     seed=seed,
+    #     config_weight_type=cfg.DATASET.WEIGHT_TYPE,
+    #     config_size_type=cfg.DATASET.SIZE_TYPE,
+    # )
+    parallel_collater = ParallelCollater(
+        # 0 if cfg["cpu"] else 1,
+        1,
+        #         # self.config["model_attributes"].get("otf_graph", False),
+    )
+    dataset = LmdbDatasetAccess(cfg["dataset"]["train"], cfg["dataset"]["val"],
+                                cfg["dataset"]["test"])
+    train_dataset = dataset.get_train()
+    val_dataset = dataset.get_valid()
+    test_dataset = dataset.get_test()
+    train_loader = DataLoader(train_dataset, collate_fn=parallel_collater,
+                              batch_size=cfg["optim"]["batch_size"],
+                              shuffle=True, pin_memory=True, )
+    val_loader = DataLoader(val_dataset, collate_fn=parallel_collater, batch_size=cfg["optim"]["batch_size"],
+                            shuffle=False, pin_memory=True, )
+    test_loader = DataLoader(test_dataset, collate_fn=parallel_collater, batch_size=cfg["optim"]["batch_size"],
+                             shuffle=False, pin_memory=True, )
+
+
+
+    # ---- training/test process ----
+    ### Repeat multiple times to get std
+    # for i in range(0, cfg.DATASET.REPEAT):
+    for i in range(0, 1):
+        seed = seed + i * 10
+        set_seed(seed)  # seed_everything in pytorch_lightning did not set torch.backends.cudnn
+        print(f"==> Building model for seed {seed} ......")
+        # ---- setup model and logger ----
+        model = load_model(cfg, dataset, cfg["model"]["output_dim"])
+        tb_logger = pl_loggers.TensorBoardLogger('./', name="seed{}".format(seed))
+
+        # TODO: make checkpoint save top k models
+
+        checkpoint_callback = ModelCheckpoint(
+            filename="{epoch}-{step}-{valid_loss:.4f}",
+            # save_last=True,
+            # save_top_k=1,
+            monitor="valid_loss",
+            mode="min",
+        )
+
+        ### Set early stopping
+        # early_stop_callback = EarlyStopping(monitor="valid_target_acc", min_delta=0.0000, patience=100, mode="max")
+
+        lr_monitor = LearningRateMonitor(logging_interval="epoch")
+        progress_bar = TQDMProgressBar(0) #PB_FRESH
+
+        ### Set the lightning trainer.
+        trainer = pl.Trainer(
+            min_epochs=0,
+            max_epochs=cfg["optim"]["max_epochs"],
+            # resume_from_checkpoint=last_checkpoint_file,
+            # accelerator="gpu" if args.devices != 0 else "cpu",
+            accelerator="gpu",
+            # devices=args.devices if args.devices != 0 else "auto",
+            devices="auto",
+            logger=tb_logger,
+            # fast_dev_run=cfg.OUTPUT.FAST_DEV_RUN,
+            callbacks=[lr_monitor, checkpoint_callback, progress_bar],
+        )
+
+        ### Find learning_rate
+        # lr_finder = trainer.tuner.lr_find(model, max_lr=0.1, min_lr=1e-6)
+        # fig = lr_finder.plot(suggest=True)
+        # fig.show()
+        # logging.info(lr_finder.suggestion())
+
+        ### Training/validation process
+        trainer.fit(model, train_loader, val_loader)
+
+        # # Load the best model
+        # best_model_path = checkpoint_callback.best_model_path
+        # best_model = get_model(cfg, dataset_train, 1).load_from_checkpoint(best_model_path)
+        #
+        # # Test process
+        # trainer.test(best_model, test_dataloader)
+
+        ### Test process
+        trainer.test(model, test_loader)
 
 
 if __name__ == "__main__":
-    setup_logging()
-
-    parser = flags.get_parser()
-    args, override_args = parser.parse_known_args()
-    config = build_config(args, override_args)
-    seed = 2020
-
-    random.seed(seed)
-    torch.manual_seed(seed)
-
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-
-
-    if args.submit:  # Run on cluster
-        slurm_add_params = config.get(
-            "slurm", None
-        )  # additional slurm arguments
-        if args.sweep_yml:  # Run grid search
-            configs = create_grid(config, args.sweep_yml)
-        else:
-            configs = [config]
-
-        logging.info(f"Submitting {len(configs)} jobs")
-        executor = submitit.AutoExecutor(
-            folder=args.logdir / "%j", slurm_max_num_timeout=3
-        )
-        executor.update_parameters(
-            name=args.identifier,
-            mem_gb=args.slurm_mem,
-            timeout_min=args.slurm_timeout * 60,
-            slurm_partition=args.slurm_partition,
-            gpus_per_node=args.num_gpus,
-            cpus_per_task=(config["optim"]["num_workers"] + 1),
-            tasks_per_node=(args.num_gpus if args.distributed else 1),
-            nodes=args.num_nodes,
-            slurm_additional_parameters=slurm_add_params,
-        )
-        for config in configs:
-            config["slurm"] = copy.deepcopy(executor.parameters)
-            config["slurm"]["folder"] = str(executor.folder)
-        jobs = executor.map_array(Runner(), configs)
-        logging.info(
-            f"Submitted jobs: {', '.join([job.job_id for job in jobs])}"
-        )
-        log_file = save_experiment_log(args, jobs, configs)
-        logging.info(f"Experiment log saved to: {log_file}")
-
-    else:  # Run locally
-        Runner()(config)
+    main()
